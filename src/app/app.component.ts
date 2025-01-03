01b918e98615a3066752eb03e1d9cbcc5d8d3ede
dailyCoder-15 <maheshwarbabar@gmail.com>
2025-01-03 10:42:39 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,79 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Password_Generator';
+
+  // Properties 
+  // variable declaration = variable name datatype and value
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  length = 0; 
+  password: string = '';
+
+  
+  onChangeLength(value: string) {
+    
+    const parsedValue = parseInt(value); 
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+      console.log(`Text Value: ${parsedValue}`);
+    }
+  }
+
+  
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+    console.log(this.includeLetters);
+    
+  }
+
+  
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+ 
+  onChangeUseSymbol() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  
+  onSubmit() {
+
+    console.log(`Entered Length is ${this.length}`);
+    
+    const numbers = '0123456789';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()+';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      //validChars += letters;
+      validChars = letters + letters;
+    }
+
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+
+    let generatePassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatePassword += validChars[index];
+    }
+
+    this.password = generatePassword;
+    
+  }
+
 }
